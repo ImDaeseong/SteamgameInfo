669aa16180b1f2ae87617c5aba71717e42beef2d
ImDaeseong <cs93059@gmail.com>
2016-11-11 12:22:39 +0900

    steam
    steam game information

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SteamgameInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(533, 290);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(12, 331);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(187, 21);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(205, 331);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(206, 21);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "steam login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 357);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "steam shutdown";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 395);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SteamgameInfo
+{
+    public partial class Form1 : Form
+    {
+        clsSteam objstm = clsSteam.getInstance;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitListView();
+        }
+
+        private void InitListView()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("appid", 80, HorizontalAlignment.Center);
+            listView1.Columns.Add("installdir", 450, HorizontalAlignment.Center);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objstm.GetGameInstallList();
+
+            //check install game
+            for (int i = 0; i < objstm.STEAM_ITEM.Count; i++)
+            {
+                string strPath = string.Format("{0}\\steamapps\\common\\{1}", objstm.GetSteamPath("SteamPath"), objstm.STEAM_ITEM[i].INSTALLDIR);
+                if (Directory.Exists(strPath))
+                {
+                    //Console.WriteLine("{0}:{1}", objstm.STEAM_ITEM[i].APPID, objstm.STEAM_ITEM[i].INSTALLDIR);
+                    ListViewItem item = new ListViewItem();
+                    item.Text = objstm.STEAM_ITEM[i].APPID;
+                    item.SubItems.Add(objstm.STEAM_ITEM[i].INSTALLDIR);
+                    listView1.Items.Add(item);
+                }
+            }  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == "" || txtPassword.Text == "")
+                return;
+
+            objstm.StartSteam(txtID.Text, txtPassword.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            objstm.StopSteam();
+        }
+    }
+}
